a21799dd7472b8d4acf0090b4462f3d34351c839
Danrley <danrleyifba@gmail.com>
2024-08-15 18:22:31 -0300

    att

---
+++ atividades/src/Q11IdadeVotar.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Q11IdadeVotar {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite a idade: ");
+        int idade = scanner.nextInt();
+
+        if (idade < 16){
+            System.out.println("Não pode votar");
+        } else if (idade>= 16 && idade <18 || idade >= 65){
+            System.out.println("Pode votar, com voto facultativo");
+        } else {
+            System.out.println("Voto obrigatório");
+        }
+        scanner.close();
+    } 
+}
